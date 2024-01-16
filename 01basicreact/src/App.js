a5c06b5be7d7a9f1ba45217f3395cbefc6a1b81c
Aniruddha Roy <65581823+ani3321r@users.noreply.github.com>
2024-01-16 13:41:21 +0530

    react basics

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Finally starting react | Aniruddha Roy </h1>;
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>react with vite | Aniruddha Roy</h1>;
+}
+
+export default App;
