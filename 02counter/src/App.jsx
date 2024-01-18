03ae7b4543b27a37d8c5374fe31ae461a94f5478
Aniruddha Roy <65581823+ani3321r@users.noreply.github.com>
2024-01-18 23:22:46 +0530

    hooks1

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let counter = 5;
+  const addValue = () => {
+    counter = counter + 1;
+  };
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button>remove value</button>
+    </>
+  );
+}
+
+export default App;
